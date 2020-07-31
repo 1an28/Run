702af70af24e15c95632171ec6ebe2fa2e215099
1an28 <14.nya.28@gmail.com>
2020-07-31 12:43:31 +0900

    set the coordinates of the camera.

---
+++ Assets/CameraCtrl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+    public GameObject palyer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float nextX;
+
+        if ( 0 >= palyer.transform.position.x ) {
+            nextX = 0;
+        } else {
+            nextX = palyer.transform.position.x;
+        }
+
+        transform.position = new Vector3( nextX, transform.position.y, transform.position.z );
+    }
+}
