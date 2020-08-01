239040101a7062f8e667c45b8771e604ce679ec2
1an28 <14.nya.28@gmail.com>
2020-08-01 10:52:37 +0900

    made a coin.

---
+++ Assets/CoinCtrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCtrl : MonoBehaviour
+{
+    public int price = 1;
+    public PlayerCtrl playerCtrl;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D( Collision2D col ) {
+        if ( col.gameObject.tag == "Player" ) {
+            playerCtrl.GetCoin( price );
+            Destroy( gameObject );
+        }
+    }
+}
--- Assets/PlayerCtrl.cs
+++ Assets/PlayerCtrl.cs
@@ -11,6 +11,9 @@ public class PlayerCtrl : MonoBehaviour
     private Animator anim;
     private SpriteRenderer spRend;
 
+    private int coin = 0;
+    private float velX;
+    private float velY;
 
     // Start is called before the first frame update
     void Start()
@@ -23,15 +26,40 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = 1.0f; //右方向に走らせ続ける
-        //Input.GetAxisRaw("Horizontal"); //A[-1] : Null[0] : D[+1] <キー入力>
+        velX = rb2d.velocity.x; //x軸の速度更新
+        velY = rb2d.velocity.y; //y軸の速度更新
 
-        float velX = rb2d.velocity.x; //x軸の速度取得
-        float velY = rb2d.velocity.y; //y軸の速度取得
+        Move(); //基本移動処理, 移動アニメーション
+
+    }
+
+    void Move() {
+        //float x = 1.0f; //右方向に走らせ続ける
+        float x = Input.GetAxisRaw("Horizontal"); //A[-1] : Null[0] : D[+1] <キー入力>
 
         anim.SetFloat( "Speed", Mathf.Abs( velX ) ); //AnimatorのSpeedに速度を代入
         rb2d.AddForce( Vector2.right * x * speed ); //rigidbody2Dの関数を使ってplayerを移動させる
 
+        Jump();
+
+        //振り向き    
+        if ( x > 0 ) {
+            spRend.flipX = false;
+        } else if ( x < 0 ) {
+            spRend.flipX = true;
+        }
+        
+        //速度制限
+        if ( velX > 8.0f ) {
+            rb2d.velocity = new Vector2( 8.0f, velY );
+        } else if ( velX < -8.0f ) {
+            rb2d.velocity = new Vector2( -8.0f, velY );
+        }
+
+    }
+
+    void Jump() {
+        //アニメーション
         if ( velY > 1.3f ) { //ジャンプアニメーション
             anim.SetBool( "isJump", true );
             anim.SetBool( "isFall", false );
@@ -43,23 +71,13 @@ public class PlayerCtrl : MonoBehaviour
             anim.SetBool( "isFall", false );
         }
 
-        //進んだ方向を向くようにする    
-        if ( x > 0 ) {
-            spRend.flipX = false;
-        } else if ( x < 0 ) {
-            spRend.flipX = true;
-        }
-
         //飛ぶ
         if ( Input.GetButtonDown("Jump") ) {
             rb2d.AddForce( Vector2.up * jumpForce );
         }
-        
-        //速度制限
-        if ( velX > 8.0f ) {
-            rb2d.velocity = new Vector2( 8.0f, velY );
-        } else if ( velX < -8.0f ) {
-            rb2d.velocity = new Vector2( -8.0f, velY );
-        }
+    }
+
+    public void GetCoin( int price ) {
+        coin += price;
     }
 }
